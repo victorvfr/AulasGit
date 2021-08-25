c59d33526dd57bf0d1b7d4e7b24eacbe1768d189
victor-vfr <vitelafr@gmail.com>
2021-08-24 21:44:37 -0300

    comitando segunda versao

--- Empregado.java
+++ Empregado.java
@@ -1,16 +1,35 @@
 public class Empregado{
-private String nome;
-private int idade;
-public String getNome (){
-return this.nome;
-}
-public void setNome (String nome){
-this.nome = nome;
-}
-public int getIdade (){
-return this.idade;
-}
-public void setIdade (int idade){
-this.idade = idade;
-}
-}
+    private String nome;
+    private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    public double calculaSalario (){
+    if (tipo == 1){
+    return salario;
+    }
+    else if (tipo == 2){
+    //comissão varia de 0 a 1, é um percentual sobre o salário
+    return salario + salario * comissao;
+    }
+    else if (tipo == 3){
+    return salario + bonus;
+    }
+    else{
+    return 0.;
+    }
+    }
+    public String getNome (){
+    return this.nome;
+    }
+    public void setNome (String nome){
+    this.nome = nome;
+    }
+    public int getIdade (){
+    return this.idade;
+    }
+    public void setIdade (int idade){
+    this.idade = idade;
+    }
+    }
